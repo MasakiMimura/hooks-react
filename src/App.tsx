be8cfd3df039dc67b06bcbfdac503c094c401217
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-07-28 08:01:35 +0900

    基本ページの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+
+const user = {
+  id: 1,
+  name: "みむ",
+  email: "123@abc.com",
+  address: "ADDRESS"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserCard user={user} />
     </div>
   );
 }
